8e2141edaea08ab402ddbab38db044b488d2abc1
sharegamer <ll1@mail.ustc.edu.cn>
2023-08-14 21:28:36 +0800

    lab3

---
+++ lab3/Flik/testFilk.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class testFilk {
+    @Test
+    public void testFilk()
+    {
+        int a=127;
+        int b=127;
+
+        assertTrue(Flik.isSameNumber(a,b));
+    }
+
+
+}
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -66,6 +66,18 @@ public class IntListTest {
         assertEquals(IntList.of(1, 2, 3), A);
     }
 
+    @Test
+    public void testreverse()
+    {
+        IntList a=IntList.of(1,2,3);
+        IntList c=IntList.of(3,2,1);
+        IntList d=null;
+        IntList.reverse(d);
+        IntList b=IntList.reverse(a);
+        assertEquals(b,a);
+        assertEquals(a,c);
+        assertEquals(d,null);
+    }
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
