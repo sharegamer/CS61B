14afbf064ae01ae2629314dc5f8771a554a4c9f5
sharegamer <ll1@mail.ustc.edu.cn>
2023-08-27 19:40:33 +0800

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,74 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    public WeightedQuickUnionUF unionset;
+    public int[][] array;
+    public int n;
+    public int opensite;
+    public Percolation(int N)                // create N-by-N grid, with all sites initially blocked
+    {
+        if(N<=0)
+            throw new java.lang.IllegalArgumentException("N must greater than 0");
+        unionset=new WeightedQuickUnionUF(N*N);
+        n=N;
+        opensite=0;
+        array=new int[N][N];
+        for(int i=0;i<N;i++)
+            for(int j=0;j<N;j++)
+                array[i][j]=0;
+    }
+    public void open(int row, int col)       // open the site (row, col) if it is not open already
+    {
+        if(row<0 || row>=n || col<0 || col>=n)
+            throw new IndexOutOfBoundsException("out of index");
+        array[row][col]=1;
+        if(row>0)
+            if(array[row-1][col]==1)
+                unionset.union(row*n+col,row*n+col-n);
+        if(row<n-1)
+            if(array[row+1][col]==1)
+                unionset.union(row*n+col,row*n+col+n);
+        if(col>0)
+            if(array[row][col-1]==1)
+                unionset.union(row*n+col,row*n+col-1);
+        if(col<n-1)
+            if(array[row-1][col+1]==1)
+                unionset.union(row*n+col,row*n+col+1);
+        opensite++;
+    }
+    public boolean isOpen(int row, int col)  // is the site (row, col) open?
+    {
+        if(row<0 || row>=n || col<0 || col>=n)
+            throw new IndexOutOfBoundsException("out of index");
+        if(array[row][col]==1)
+            return true;
+        return false;
+    }
+    public boolean isFull(int row, int col)  // is the site (row, col) full?
+    {
+        if(row<0 || row>=n || col<0 || col>=n)
+            throw new IndexOutOfBoundsException("out of index");
+        for (int i=0;i<n;i++)
+        {
+            if(unionset.connected(row*n+col,col))
+                return true;
+        }
+        return false;
+    }
+    public int numberOfOpenSites()           // number of open sites
+    {
+        return opensite;
+    }
+    public boolean percolates()              // does the system percolate?
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(isFull(n,i))
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args)   // use for unit testing (not required)
+    {}
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,55 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
+import java.util.Random;
 
 public class PercolationStats {
+    public double mean;
+    public double stddev;
+    public double low;
+    public double high;
+    public PercolationStats(int N, int T, PercolationFactory pf)   // perform T independent experiments on an N-by-N grid
+    {
 
+        int[] array=new int[T];
+        int i,j;
+        for(int i=0;i<T;i++)
+        {
+            int num=0;
+            Percolation p=pf.make(N);
+            while(!p.percolates())
+            {
+                i=StdRandom.uniform(N);
+                j=StdRandom.uniform(N);
+                if(p.isOpen(i,j))
+                    continue;
+                p.open(i,j);
+                num++;
+                if(p.percolates())
+                    break;
+            }
+            array[i]=num;
+        }
+        mean=StdStats.mean(array);
+        stddev=StdStats.stddev(array);
+        low=mean-1.96*stddev/Math.sqrt(T);
+        high=mean+1.96*stddev/Math.sqrt(T);
+    }
+    public double mean()                                           // sample mean of percolation threshold
+    {
+        return mean;
+    }
+    public double stddev()                                         // sample standard deviation of percolation threshold
+    {
+        return stddev;
+    }
+    public double confidenceLow()                                  // low endpoint of 95% confidence interval
+    {
+        return low;
+    }
+    public double confidenceHigh()                                 // high endpoint of 95% confidence interval
+    {
+        return high;
+    }
 }
